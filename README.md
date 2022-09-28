3dde7fc4390276acf825327452f69ea6063c0419
thiamhooi <thiamhooi.th@gmail.com>
2022-09-28 13:06:18 +0800

    Init version from navigation;

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# android-kotlin-template-navigation-viewmodel
+# Navigation Template
+
+This app has the base navigation template to ease your initial development. By using `BottomNavigationView` and `Navigation Graph`, the app is able to navigate to different menu or to sub-screen through `Fragment`.
+
+Other available examples:
++ Within `Fragment`, accessing the binding view (`Button` `TextView`) from the `xml` layout.
+
+### Example of menu structure
+
++ `Menu` `Nested navigation graph` Bold
+	+ `Sub Screen` Bold 1
+	+ `Sub Screen` Bold 2
+	+ `Navigate` Italic menu
+	+ `Button` Click to change binding field text name
++ `Menu` Italic
++ `Menu` Parking
++ `Menu` Title
+
+## Navigation Graph
+![](https://github.com/ff55lab/android-kotlin-template-navigation/raw/main/nav_graph_1.png)
+
+## Nested Navigation Graph
+![](https://github.com/ff55lab/android-kotlin-template-navigation/raw/main/nav_graph_2_nested.png)
+
+## Navigation UI
+
+![](https://github.com/ff55lab/android-kotlin-template-navigation/raw/main/navigation_ui.png)
+
+## Tools
+
+- Android Studio Chipmunk | 2021.2.1 Patch 2
+- Android SDK API Level 31 (Android 12.0)
+- JDK 11.0.15 (64-bit)
---
+++ app/src/main/java/com/example/navigation/ui/BoldFragment.kt
@@ -0,0 +1,67 @@
+package com.example.navigation.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigation.MainActivity
+import com.example.navigation.R
+import com.example.navigation.databinding.FragmentBoldBinding
+
+class BoldFragment : Fragment() {
+
+    private var _binding: FragmentBoldBinding? = null
+    private lateinit var _main: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        _main = requireActivity() as MainActivity
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        val fragmentBinding = FragmentBoldBinding.inflate(inflater, container, false)
+        _binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding?.boldFragment = this
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun showBold1() {
+        findNavController().navigate(R.id.action_fragmentBold_to_bold1Fragment)
+    }
+
+    fun showBold2() {
+        findNavController().navigate(R.id.action_fragmentBold_to_bold2Fragment)
+    }
+
+    fun showItalic() {
+        _main.binding.bottomNavigationView.selectedItemId = R.id.fragmentItalic
+    }
+
+    //region Example: Binding view
+
+    fun showClickMe() {
+        // _binding
+        // Accesses all binding views with valid android:id
+
+        // _binding?.clickMe
+        // Accesses to clickMe Button view
+
+        _binding?.clickMe?.text = getString(R.string.done_binding_me)
+    }
+
+    //endregion
+
+}
